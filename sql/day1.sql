ae5b1c5abeeb8e4c1169ca39bdabce45e7929d17
wujiandong66 <15652618578@163.com>
2017-05-15 17:31:00 +0800

    Initial commit

---
+++ sql/day1.sql
@@ -0,0 +1,11 @@
+SHOW DATABASES ;
+CREATE DATABASE db_1702;
+DROP DATABASE db_1702;
+USE db_1702;
+SHOW TABLES ;
+CREATE TABLE t_student(
+  sno VARCHAR(10),
+  sname VARCHAR(10),
+  
+  age int(2)
+)
